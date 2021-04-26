a2f4fe0e401bacdbe80cc6d13c2e820f1263c2f7
antonio machado <macchado@users.noreply.github.com>
2021-04-26 16:49:06 -0300

    Criando página de exemplo PHP com HTML

---
+++ teste.php
@@ -0,0 +1,19 @@
+<?php
+
+function nomeDaFuncao($parametro)
+{
+	echo "Olá: ".$parametro;
+}
+
+nomeDaFuncao("Fulano");
+
+?>
+<html dir="ltr" lang="pt">
+  <head>
+    <meta charset="utf-8">
+    <title>Titulo</title> 
+  </head>
+  <body>
+       Texto da minha página.
+  </body>  
+</html>
